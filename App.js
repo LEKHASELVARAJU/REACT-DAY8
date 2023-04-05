d52a0ca872636df514b59e781adf6b3851de6b77
LEKHASELVARAJU <108514177+LEKHASELVARAJU@users.noreply.github.com>
2023-04-05 10:22:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+// import './App.css';
+// import App1 from './component/Hook';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <App1/>
+//     </div>
+//   );
+// }
+
+// export default App;
+// import './App.css';
+// import Productlist from './Product';
+
+// function App() {
+
+//   return (
+//     <div className="App">
+//       <Productlist products={[{no:"1",name:"Phone", price:"25000", img:"phone.jpeg"},{no:"2",name:"Car", price:"1500000", img:"Car.png"}]}/>
+//     </div>
+//   );
+// }
+
+// export default App;
